9b0da8c9da3124a6a22d7c5c44e888992d4d31fa
Ric Wright <rkwright@geofx.com>
2023-11-07 11:53:01 -0800

    Added some functionality to Contour.js, but not the correct stuff.  Still, it's working, more or less.

---
+++ Contour.js
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+
+'use strict';
+
+/**
+ * Constants
+ */
+class Contour  {
+
+    //--- constants ---
+    REVISION = '1.0.0';
+
+    //--- class methods ---
+    constructor () {
+    }
+
+    SetUp ( array ) {
+
+    }
+
+    ThreadContours ( array,  contLevel ) {
+
+    }
+}
