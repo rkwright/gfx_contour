412b9416f4aebc8957b72a0a66e468cc0a855e76
Ric Wright <rkwright@geofx.com>
2023-11-30 16:56:44 -0800

    More progress towards a clean build Not there yet but progress.  But this is the last commit before the major restructuring.

--- Contour.js
+++ Contour.js
@@ -31,7 +31,7 @@ const TOP_EDGE = 1;
 const RIGHT_EDGE = 2;
 const BOTTOM_EDGE = 3;
 
-const TOP_BOT_EDGE = 1;		// if edg & this, then TOP or BOTTOM
+//const TOP_BOT_EDGE = 1;		// if edg & this, then TOP or BOTTOM
 
 const EPSILON = 0.0005;
 
@@ -43,16 +43,15 @@ const id = [ 2, 3, 0, 1 ];
 
 /**
  *
- * @author riwright
  *
  */
 class ContourLimit {
     top0 = MAX_LOOP_LIMIT;	 	// Contour limits for a given cell, limb 0
     bot0 = MAX_LOOP_LIMIT;
-    top1 = MAX_LOOP_LIMIT; 		// Contour limits for a given cell, limb 1
+    top1 = MAX_LOOP_LIMIT;
     bot1 = MAX_LOOP_LIMIT;
-    CW0  = UNDEFINED; 		// sign of slope of limb intersected by vector
-    CW1  = UNDEFINED; 		// sign of slope of limb intersected by vector
+    CW0  = UNDEFINED; 		   // sign of slope of limb intersected by vector
+    CW1  = UNDEFINED;
 }
 
 /**
@@ -96,7 +95,7 @@ class ContourVector {
 /**
  *
  */
-class Contour  {
+class Contour {
 
     //--- constants ---
     REVISION = '0.2.0';
@@ -105,6 +104,8 @@ class Contour  {
     nf; 			// last index in X
     ms; 			// first index in Y
     mf; 			// last index in Y
+    minZ;
+    maxZ;
     ccwKnt; 		// accumulates the CCW/CW count
     nCont;			// number of contours to be found
 
@@ -115,7 +116,7 @@ class Contour  {
     /**
      * Required constructor, currently unused
      */
-    constructor () {
+    constructor() {
     }
 
     /**
@@ -123,10 +124,10 @@ class Contour  {
      * @param array
      * @param contLevel
      */
-    threadContours ( array,  contLevel ) {
+    threadContours ( array, contLevel ) {
 
         console.log("Threading Contour. Array size: " + array.length +
-                            "  contour level: " + contLevel.toFixed(1));
+            "  contour level: " + contLevel.toFixed(1));
     }
 
     /**
@@ -141,123 +142,414 @@ class Contour  {
         this.ms = 0;
 
         this.bounds = [];
-        for ( let i = ms; i < mf; i++ ) {
+        for ( let i = this.ms; i < this.mf; i++ ) {
             this.bounds[i] = [];
-            for ( let j = ns; j < nf; j++ ) {
+            for ( let j = this.ns; j < this.nf; j++ ) {
                 this.bounds[i][j] = new ContourLimit();
             }
         }
-
-        this.nCont = Math.ceil((maxZ - minZ) / contInterval);
-
-        for ( let i = 0; i < this.nCont; i++ )
-        {
-            this.contLevels.push(minZ + i * contInterval);
-        }
-
-        this.delta = (maxZ - minZ) / DELTA_DIV;
     }
 
     /**
      *
      * @param array
-     * @param contInterval
      */
-    setContLevels ( array, contInterval ) {
-        // find max and min in the array
-        let minZ = MAX_FLOAT;
-        let maxZ = -MAX_FLOAT;
+    setContLevels ( array ) {
 
-        for (let i = ms; i < mf; i++) {
-            for (let j = ns; j < nf; j++) {
-                minZ = Math.min(minZ, array[i][j]);
-                maxZ = Math.max(maxZ, array[i][j]);
+        // find max and min in the array
+        this.minZ = MAX_FLOAT;
+        this.maxZ = -MAX_FLOAT;
+
+        for ( let i = this.ms; i < this.mf; i++ ) {
+            for ( let j = this.ns; j < this.nf; j++ ) {
+                this.minZ = Math.min(this.minZ, array[i][j]);
+                this.maxZ = Math.max(this.maxZ, array[i][j]);
             }
         }
 
-        /**
-         * @param array
-         * @param contInterval
-         *
-         * Initialize the flag array, setting the upper and lower bounds
-         * for the contours for each segment as well as the winding direction
-         */
-        initFlags(array)
-        {
-            let t, b;
-            let u, v;
-            let upLim = contLevels.length - 1;
+        this.nCont = Math.ceil((this.maxZ - this.minZ) / contInterval);
 
-            for (let i = ms; i < mf; i++) {
-                for (let j = ns; j < nf; j++) {
-                    let bound = bounds[i][j];
+        for ( let i = 0; i < this.nCont; i++ ) {
+            this.contLevels.push(this.minZ + i * contInterval);
+        }
 
-                    u = array[i][j];
+        this.delta = (this.maxZ - this.minZ) / DELTA_DIV;
+    }
 
-                    if (j < (nf - 1)) {
-                        v = array[i][j + 1];
+    /**
+     * @param array
+     *
+     * Initialize the flag array, setting the upper and lower bounds
+     * for the contours for each segment as well as the winding direction
+     */
+    initFlags ( array ) {
+        let t, b;
+        let u, v;
+        let upLim = this.contLevels.length - 1;
 
-                        //  Here we set the slope-sign for this horizontal segment.
-                        //  We set it in terms of the X-direction vector which will intersect
-                        //  this edge when the vector is going in the direction of increasing X. It
-                        //  will have the opposite sign if the vector is going in the direction
-                        //  of decreasing X
-                        bound.CW0 = (v < u) ? COUNTERCLOCKWISE : CLOCKWISE;
+        for (let i = this.ms; i < this.mf; i++) {
+            for (let j = this.ns; j < this.nf; j++) {
+                let bound = this.bounds[i][j];
 
-                        if (v > u) {
-                            t = upLim;
-                            while (t > 0 && contLevels.get(t) > v) t--;
+                u = array[i][j];
 
-                            b = 0;
-                            while (b <= t && contLevels.get(b) <= u) b++;
-                        } else {
-                            t = upLim;
-                            while (t > 0 && contLevels.get(t) > u) t--;
+                if (j < (this.nf - 1)) {
+                    v = array[i][j + 1];
 
-                            b = 0;
-                            while (b <= t && contLevels.get(b) <= v) b++;
-                        }
+                    //  Here we set the slope-sign for this horizontal segment.
+                    //  We set it in terms of the X-direction vector which will intersect
+                    //  this edge when the vector is going in the direction of increasing X. It
+                    //  will have the opposite sign if the vector is going in the direction
+                    //  of decreasing X
+                    bound.CW0 = (v < u) ? COUNTERCLOCKWISE : CLOCKWISE;
 
-                        if (t >= b) {
-                            bound.top0 = t;
-                            bound.bot0 = b;
-                        }
+                    if (v > u) {
+                        t = upLim;
+                        while (t > 0 && this.contLevels[t] > v) t--;
+
+                        b = 0;
+                        while (b <= t && this.contLevels[b] <= u) b++;
+                    } else {
+                        t = upLim;
+                        while (t > 0 && this.contLevels[t] > u) t--;
+
+                        b = 0;
+                        while (b <= t && this.contLevels[b] <= v) b++;
                     }
 
-                    if (i < (mf - 1)) {
-                        v = array[i + 1][j];
+                    if (t >= b) {
+                        bound.top0 = t;
+                        bound.bot0 = b;
+                    }
+                }
 
-                        // Now we set the slope-sign for this vertical segment.  We set
-                        // it for the vector that will intersect this edge when the
-                        // vector is going in the direction of increasing Y. It will
-                        // have the opposite sign if the vector is moving in
-                        // the direction of decreasing Y.
+                if (i < (this.mf - 1)) {
+                    v = array[i + 1][j];
 
-                        bound.CW1 = (v > u) ? COUNTERCLOCKWISE : CLOCKWISE;
+                    // Now we set the slope-sign for this vertical segment.  We set
+                    // it for the vector that will intersect this edge when the
+                    // vector is going in the direction of increasing Y. It will
+                    // have the opposite sign if the vector is moving in
+                    // the direction of decreasing Y.
 
-                        if (v > u) {
-                            t = upLim;
-                            while (t > 0 && contLevels.get(t) > v) t--;
+                    bound.CW1 = (v > u) ? COUNTERCLOCKWISE : CLOCKWISE;
 
-                            b = 0;
-                            while (b <= t && contLevels.get(b) <= u) b++;
-                        } else {
-                            t = upLim;
-                            while (t > 0 && contLevels.get(t) > u) t--;
+                    if (v > u) {
+                        t = upLim;
+                        while (t > 0 && this.contLevels[t] > v) t--;
 
-                            b = 0;
-                            while (b <= t && contLevels.get(b) <= v) b++;
-                        }
+                        b = 0;
+                        while (b <= t && this.contLevels[b] <= u) b++;
+                    } else {
+                        t = upLim;
+                        while (t > 0 && this.contLevels[t] > u) t--;
 
-                        if (t >= b) {
-                            bound.top1 = t;
-                            bound.bot1 = b;
-                        }
+                        b = 0;
+                        while (b <= t && this.contLevels[b] <= v) b++;
+                    }
+
+                    if (t >= b) {
+                        bound.top1 = t;
+                        bound.bot1 = b;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @param array             the data array itself
+     * @param contourNum        index of level of Contour
+     * @param contourLevel      actual level of Contour
+     * @return
+     */
+    singleContour ( array,
+                    contourNum,
+                    contourLevel ) {
+
+        let bExit = false;
+        let bStart = true;
+        let bEdg = true;
+        let bInRange = true;
+        let bCont = false;
+        let dt = 0;
+        let d0 = 0;
+        let direc = 0;
+        let lmb = 0;
+        let xmax = this.nf - 1.0;
+        let ymax = this.mf - 1.0;
+        let vecTop = 0;
+        let x0 = 0;
+        let x1 = this.ns;
+        let x2 = 0;
+        let xlmb = 0;
+        let y0 = 0;
+        let y1 = this.ms;
+        let y2 = 0;
+        let ylmb = 0;
+        let u, v, tt;
+        let m1, m2;
+        let ccwknt = 0;
+        let ccwval = 0;
+        let delt;
+        let bound = null;
+
+        let contVec = new ContourVector();
+
+        while (!bExit) {
+            bInRange = true;
+
+            x2 = x1 + xdirec[direc];
+
+            if (x2 < this.ns || x2 >= this.nf)
+                bInRange = false;
+            else {
+                /* Y - range OK */
+                y2 = y1 + ydirec[direc];
+
+                if (y2 < this.ms || y2 >= this.mf)
+                    bInRange = false;
+            }
+
+            if (bInRange) {     	/* set xlmb,ylmb vars */
+
+                bCont = false;
+
+                if (x1 === x2) {
+                    lmb = VERTICAL;
+                    xlmb = x2;
+                    ylmb = (y2 > y1) ? y1 : y2;
+                    bound = this.bounds[ylmb][xlmb];
+                    bCont = bound.bot1 === contourNum;
+                } else {
+                    lmb = HORIZONTAL;
+                    ylmb = y1;
+                    xlmb = (x2 > x1) ? x1 : x2;
+                    bound = this.bounds[ylmb][xlmb];
+                    bCont = bound.bot0 === contourNum;
+                }
+
+                // if there is one, then find it
+                if (bCont) {
+                    m1 = array[y1][x1];
+                    m2 = array[y2][x2];
+
+                    if (Math.abs(contourLevel - m1) <= this.delta)
+                        m1 += ((m2 > m1) ? this.delta : -this.delta);
+
+                    if (Math.abs(contourLevel - m2) <= this.delta)
+                        m2 += ((m1 > m2) ? this.delta : -this.delta);
+
+                    if (Math.abs(m2 - m1) < MIN_FLOAT)
+                        tt = 0.0;
+                    else
+                        tt = (contourLevel - m1) / (m2 - m1);
+
+                    if (Math.abs(tt) >= 1.0)
+                        tt = (1.0 - this.delta) * this.fpsign(tt);
+
+                    u = (x2 - x1) * tt + x1;
+                    v = (y2 - y1) * tt + y1;
+
+                    // store the result
+                    contVec.x.push(u);
+                    contVec.y.push(v);
+                    vecTop++;
+
+                    //conole.log(String.format("Found result: %d, %6.2f %6.2f for level: %6.2f", vecTop-1, u,v, contourLevel));
+
+                    // if the first elm, then set the entry slope value.
+                    // Note that we have to determine which direction we
+                    // are passing through the limb.
+                    if (vecTop === 1) {
+                        if (lmb === HORIZONTAL)
+                            ccwval = ((contVec.x[0] > 0) ? -bound.CW0 : bound.CW0);
+                        else
+                            ccwval = ((contVec.y[0] > 0) ? -bound.CW1 : bound.CW1);
+
+                        contVec.stCW = ccwval;
+                        contVec.stEdge = this.FindEdge(contVec.x.get(0), contVec.y.get(0), xmax, ymax);
+                    }
+
+                    ccwknt += ccwval;
+
+                    // mark this seg as "used"
+                    if (lmb !== HORIZONTAL) {
+                        bound.bot1++;
+                        if (bound.bot1 > bound.top1)
+                            bound.bot1 = MAX_LOOP_LIMIT;
+                    } else {
+                        bound.bot0++;
+
+                        if (bound.bot0 > bound.top0)
+                            bound.bot0 = MAX_LOOP_LIMIT;
                     }
                 }
             }
 
-        }
+            if (!bStart) {
+                if (bInRange) {
+                    if (bCont) {
+                        dt = id[direc];
+                        direc = nsd[direc];
+                    } else {  /* no contours found... */
+                        direc = nexd[direc];
+                        if (direc === dt) {
+                            //  back going in same dir no contour found, it must
+                            //  be closed, so dup ends so curve closes
+
+                            // save the point
+                            contVec.x.add(contVec.x.get(0));
+                            contVec.y.add(contVec.y.get(0));
+                            vecTop++;
+
+                            //System.out.println(String.format("Closed cont, duped end: %d, %6.2f %6.2f", vecTop - 1, contVec.x.get(0), contVec.y.get(0)));
+
+                            // signal that this is closed and set the direction flag
+                            contVec.stCW = CLOSED;
+                            contVec.finCW = (ccwknt < 0) ? COUNTERCLOCKWISE : CLOCKWISE;
+                            ccwknt = 0;
+
+                            this.contourVectors.push(contVec);
+
+                            vecTop = 0;
+                            contVec = new ContourVector();
+
+                            // go back to where last Contour started and begin again
+                            direc = d0;
+                            x1 = x0 + xdirec[direc];
+                            y1 = y0 + ydirec[direc];
+                            bStart = true;
+
+                            contVec.finCW = CLOSED;
+                        } else {
+                            x1 = x2;
+                            y1 = y2;
+                        }
+                    }   //cont false
+                } else {  /* out of range... */
+                    /*
+                     *	We've reached the edge, so we need to figure out what
+                     *  the slope is of the limb we are exiting through.
+                     *  Note that we have to determine which direction we
+                     *  we are passing through the limb.
+                     */
+                    if (lmb === HORIZONTAL)
+                        contVec.finCW = ((contVec.x[vecTop - 1] < contVec.x[vecTop - 2]) ?
+                            -bound.CW0 : bound.CW0);
+                    else
+                        contVec.finCW = ((contVec.y[vecTop - 1] < contVec.y[vecTop - 2]) ?
+                            -bound.CW1 : bound.CW1);
+
+                    contVec.finEdge = this.FindEdge(contVec.x[vecTop - 1], contVec.y[vecTop - 1], xmax, ymax);
+
+                    this.contourVectors.add(contVec);
+
+                    vecTop = 0;
+                    contVec = new ContourVector();
+
+                    direc = d0;
+
+                    x1 = x0 + xdirec[direc];
+                    y1 = y0 + ydirec[direc];
+
+                    bStart = true;
+
+                    contVec.finCW = CLOSED;
+                }
+            }     /* end of if !bStart */
+            else {
+                /* if bStart */
+                if (bInRange) {
+                    if (bCont) {
+                        // found a contour, this is first cell, so
+                        // save  current cell coords, direction
+
+                        x0 = x1;
+                        y0 = y1;
+                        d0 = direc;
+                        x1 = x2;
+                        y1 = y2;
+                        dt = direc;
+
+                        bStart = false;
+
+                        direc = nexd[direc];
+                    } else {
+                        x1 = x2;
+                        y1 = y2;
+                    }
+                } else {  /* out of range... */
+
+                    if (bEdg) {
+                        if (y2 < this.ms) {
+                            x1 = this.ns;
+                            y1 = this.ms + 1;
+                            direc = 0;
+                            bEdg = false;
+                        } else
+                            direc = nexd[direc];
+                    } else {
+                        if (direc === 1) {
+                            y1 = this.ms;
+                            x1++;
+                            bExit = (x1 >= this.nf);
+                        } else {
+                            y1++;
+                            x1 = this.ns;
+                            if (y1 >= this.mf) {
+                                x1 = this.ns + 1;
+                                y1 = this.ms;
+                                direc = 1;
+                            }
+                        }
+                    }
+                }
+            }    // elseif bStart
+
+        }	// while
+
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param xmax
+     * @param ymax
+     * @return
+     */
+    FindEdge ( x, y, xmax, ymax ) {
+        let edg = LEFT_EDGE;
+
+        if (this.fpnear(x, xmax))
+            edg = RIGHT_EDGE;
+        else if (this.fpnear(y, ymax))
+            edg = TOP_EDGE;
+        else if (this.fpnear(y, 0))
+            edg = BOTTOM_EDGE;
+
+        return edg;
+    }
+
+    /**
+     *
+     * @param arg
+     * @return
+     */
+    fpsign(arg) {
+        return (arg < 0.0) ? -1.0 : 1.0;
+    }
+
+    /**
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    fpnear(a, b) {
+        return (Math.abs(a - b) < EPSILON);
     }
 }
 
